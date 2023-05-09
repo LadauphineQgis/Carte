e8490e3dde02d1448bbc177121b6cf3f8680a530
LadauphineQgis <132495225+LadauphineQgis@users.noreply.github.com>
2023-05-10 00:15:28 +0200

    Update Lesvoisins_1_style.js

--- styles/Lesvoisins_1_style.js
+++ styles/Lesvoisins_1_style.js
@@ -21,7 +21,7 @@ var style_Lesvoisins_1 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(244,6,26,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(244,6,26,0.5)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
