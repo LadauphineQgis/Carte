eb78bd102b219ccd0e6d733b4d364b7e5ae3a8b4
LadauphineQgis <132495225+LadauphineQgis@users.noreply.github.com>
2023-05-10 00:11:32 +0200

    Update Accueil_15_style.js

--- styles/Accueil_15_style.js
+++ styles/Accueil_15_style.js
@@ -21,7 +21,7 @@ var style_Accueil_15 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(128,14,16,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(106,159,228,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(128,14,16,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(106,159,228,0.5)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
