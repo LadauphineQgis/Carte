0c7b6de785675e2909195ad6712cdce47bdbdb85
LadauphineQgis <132495225+LadauphineQgis@users.noreply.github.com>
2023-05-10 00:16:38 +0200

    Update VCBParking_4_style.js

--- styles/VCBParking_4_style.js
+++ styles/VCBParking_4_style.js
@@ -21,7 +21,7 @@ var style_VCBParking_4 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(235,55,255,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(235,55,255,0.5)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
