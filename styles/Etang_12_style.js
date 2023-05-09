7dd70ba7f4816d3ab0421ac570edb040da8d171a
LadauphineQgis <132495225+LadauphineQgis@users.noreply.github.com>
2023-05-10 00:14:37 +0200

    Update Etang_12_style.js

--- styles/Etang_12_style.js
+++ styles/Etang_12_style.js
@@ -21,7 +21,7 @@ var style_Etang_12 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(128,14,16,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(124,228,133,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(128,14,16,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(124,228,133,0.5)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
