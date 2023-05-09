04fb390de140935c27f35cfb71ad2be643526688
LadauphineQgis <132495225+LadauphineQgis@users.noreply.github.com>
2023-05-10 00:14:53 +0200

    Update Hautballet_7_style.js

--- styles/Hautballet_7_style.js
+++ styles/Hautballet_7_style.js
@@ -21,7 +21,7 @@ var style_Hautballet_7 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(182,108,190,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(182,108,190,0.5)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
