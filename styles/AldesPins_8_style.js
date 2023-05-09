a1a39c0d0ffecd592664ff457ec1a975413d0377
LadauphineQgis <132495225+LadauphineQgis@users.noreply.github.com>
2023-05-10 00:12:42 +0200

    Update AldesPins_8_style.js

--- styles/AldesPins_8_style.js
+++ styles/AldesPins_8_style.js
@@ -21,7 +21,7 @@ var style_AldesPins_8 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(71,234,188,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(71,234,188,0.5)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
